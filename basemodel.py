cfdbb39995f082687091436db6f46b510d13777c
Pavel Kochkin <54332465+pavelkochkin1@users.noreply.github.com>
2021-08-15 17:00:40 +0500

    basemodel upload

--- basemodel.py
+++ basemodel.py
@@ -4,9 +4,10 @@ import joblib
 import sys
 from nltk.stem import SnowballStemmer
 
+def tokenize(host: str):
+    '''Tokenizer for string with host'''
 
-def tokenize(url: str):
-    tokens = url.split(sep='.')
+    tokens = host.split(sep='.')
     tokens = [i for i in tokens if i not in string.punctuation]
     tokens = [snowball.stem(i) for i in tokens]
     return tokens
@@ -21,11 +22,12 @@ def main():
 
     host = [sys.argv[1]]
     prediction = model.predict(host).tolist()
+    proba = model.predict_proba(host)[0,1]
 
     if prediction == [1]:
-        result = json.dumps({"class": 'human'})
+        result = json.dumps({"class": 'human', "proba": proba})
     else:
-        result = json.dumps({"class": 'tech'})
+        result = json.dumps({"class": 'tech', "proba": proba})
     print(result)
 
 if __name__ == "__main__":
